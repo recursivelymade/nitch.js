aae7c49c9c9f4805a438f76a0db4649d6d8fd2f7
Pete <pete@menace.co.nz>
2012-07-16 19:51:42 +0100

    Removing code that was breaking the build

--- src/music.js
+++ src/music.js
@@ -1,28 +1,8 @@
 /**
  * @namespace nitch.music
- * @description This is a placeholder under we can think of a better way deliver a consistent
+ * @description This is a placeholder so we can think of a better way deliver a consistent
  * and timely music control system. It should load the details of audio via a json file
  * and apply the right file type dependant on support from the browser
 **/
 nitch.music = function() {
-	return false;
-	
-	var music = {}
-	
-	load: function() {
-	
-	}
-	
-	for (var track in musicConfig){
-		music[track] = new Audio();
-		music[track].src = "/audio/1.1/"+musicConfig[track] + ".mp3";
-		// Need to reset the time to replay the audio, annoying huh?
-		music[track].addEventListener('ended', function() {
-		// reloads the audio so can play it again
-			this.load();
-		}, false);
-	}
-	
-	return sfx;
-	
 }
