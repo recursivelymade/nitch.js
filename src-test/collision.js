86fac0a2bee1755545484377ac66ce5a36e4ac10
Pete <pete@menace.co.nz>
2012-08-23 16:05:22 +0100

    Test for when objects touch

--- src-test/collision.js
+++ src-test/collision.js
@@ -35,3 +35,23 @@ collisions.prototype.testCollide = function() {
 	assertEquals(95, collide.pv[0]);
 	assertEquals(-110, collide.pv[1]);
 }
+
+collisions.prototype.testTouching = function() {
+	var touch = nitch.collision.AABB({
+		width: 200,
+		height: 100,
+		x: 30,
+		y: 50	
+		}, {
+		width: 200,
+		height: 100,
+		x: 230,
+		y: 150
+	});
+	
+	assertObject(touch);
+	assertEquals('bottom', touch.dir);
+	assertArray(touch.pv);
+	assertEquals(0, touch.pv[0]);
+	assertEquals(0, touch.pv[1]);
+}
